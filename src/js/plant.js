f5fef1b06263f1c1a6082e7459e96fae24866ace
Jennifer Bordon <jennifer.bordon@gmail.com>
2021-08-31 10:47:16 -0700

    add second plant and yuckyFood
    Co-authored-by: dqveha <dqveha@users.noreply.github.com>

--- src/js/plant.js
+++ src/js/plant.js
@@ -26,6 +26,7 @@ const changeState = (prop) => {
 
 const feed = changeState("soil")(1);
 const blueFood = changeState("soil")(5);
+const yuckyFood = changeState("soil")(-5);
 
 const hydrate = changeState("water")(1);
 const superWater = changeState("water")(5);
@@ -34,16 +35,40 @@ $(document).ready(function () {
 
   // This function has side effects because we are using jQuery. Manipulating the DOM will always be a side effect. Note that we only use one of our functions to alter soil. You can easily add more.
 
-  $('#feed').click(function () {
+  $('#feedA').click(function () {
     const newState = stateControl(blueFood);
-    $('#soil-value').text(`Soil: ${newState.soil}`);
+    $('#soil-value-a').text(`Soil: ${newState.soil}`);
   });
 
   // This function doesn't actually do anything useful in this application - it just demonstrates how we can "look" at the current state (which the DOM is holding anyway). However, students often do need the ability to see the current state without changing it so it's included here for reference.
 
-  $('#show-state').click(function () {
+  $('#show-stateA').click(function () {
     // We just need to call stateControl() without arguments to see our current state.
     const currentState = stateControl();
-    $('#soil-value').text(`Soil: ${currentState.soil}`);
+    $('#soil-value-a').text(`Soil: ${currentState.soil}`);
   });
+
+  $('#negativeA').click(function () {
+    const newState = stateControl(yuckyFood);
+    $('#soil-value-a').text(`Soil: ${newState.soil}`);
+  });
+
+
+  // #2 Plant
+  $('#feedB').click(function () {
+    const newState = stateControl(blueFood);
+    $('#soil-value-b').text(`Soil: ${newState.soil}`);
+  });
+
+  $('#show-stateB').click(function () {
+    const currentState = stateControl();
+    $('#soil-value-b').text(`Soil: ${currentState.soil}`);
+  });
+
+  $('#negativeB').click(function () {
+    const newState = stateControl(yuckyFood);
+    $('#soil-value-b').text(`Soil: ${newState.soil}`);
+  });
+
+
 });
